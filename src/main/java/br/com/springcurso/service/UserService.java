3302ef803c783b9dfee3f23888283ffdb81f25e8
Marcos Michel Luciano <mmluciano@hotmail.com>
2020-12-15 16:40:04 -0300

    Criado o user service

---
+++ src/main/java/br/com/springcurso/service/UserService.java
@@ -0,0 +1,51 @@
+package br.com.springcurso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.springcurso.Util.HashUtil;
+import br.com.springcurso.model.User;
+import br.com.springcurso.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User save(User user) {
+		String hash = HashUtil.getSecurehash(user.getPassword());
+		user.setPassword(hash);
+		
+		User createdUser = userRepository.save(user);
+		return createdUser;
+	}
+	
+	public User update (User user) {
+		String hash = HashUtil.getSecurehash(user.getPassword());
+		user.setPassword(hash);
+				
+		User updatedUser = userRepository.save(user);
+		return updatedUser;
+	}
+	
+	public User getById(Long id) {
+		Optional<User> result = userRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<User> listAll(){
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+	
+	public User Login(String email, String password) {
+		password = HashUtil.getSecurehash(password);
+			
+		Optional<User> result = userRepository.login(email, password);
+		return result.get();
+	}
+}

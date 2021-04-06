decba18f9efb2f41dba420a594ed8fa91eba0f45
Marcos Michel Luciano <mmluciano@hotmail.com>
2021-04-06 16:36:09 -0300

    Configuração deployable para arquivo war

--- src/main/java/br/com/springcurso/SpringCursoApplication.java
+++ src/main/java/br/com/springcurso/SpringCursoApplication.java
@@ -2,10 +2,16 @@ package br.com.springcurso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCursoApplication {
+public class SpringCursoApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringCursoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCursoApplication.class, args);
 		
